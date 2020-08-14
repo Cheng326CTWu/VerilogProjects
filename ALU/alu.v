144bc46b756554baf3b90c84cb87686e4d3d00c9
Owner (Ben Wu) <benny.wuu@gmail.com>
2020-08-14 14:45:11 -0400

    Added gitignore, finished alu module

--- ALU/alu.v
+++ ALU/alu.v
@@ -1,1 +1,33 @@
+module alu
+(
+	clk,
+	a,
+	b,
+	func,
+	shamt,
+	res
+);
 
+	parameter n = 32;
+
+	input clk;
+	input [n-1:0] a, b;
+	input [2:0] func;
+	input [4:0] shamt;
+	output reg [n-1:0] res;
+	
+	always @(posedge clk)
+	begin
+		case (func)
+		3'b000: res = a + b;
+		3'b001: res = a - b;
+		3'b010: res = a & b;
+		3'b011: res = a | b;
+		3'b100: res = a ^ b;
+		3'b101: res = b << shamt;
+		3'b110: res = b >> shamt;
+		3'b111: res = (a < b) ? 1'b1 : 1'b0;
+		endcase
+	end
+
+endmodule
--- ALU/alu_tb.v
+++ ALU/alu_tb.v
@@ -1,1 +1,66 @@
+`timescale 1ns/1ns
 
+module alu_tb();
+reg clk;
+reg [31:0] a, b;
+reg [2:0] func;
+reg [4:0] shamt;
+wire [31:0] res;
+
+alu DUT(.clk(clk), .a(a), .b(b), .func(func), .shamt(shamt), .res(res));
+
+initial
+begin
+	clk = 1'b0;
+	forever
+	begin
+		#50;
+		clk = ~clk;
+	end
+end
+
+initial
+begin
+	a = 32'd3;
+	b = 32'd2;
+	func = 3'b000;
+	shamt = 5'd0;
+
+	#100;
+	func = 3'b001;
+	shamt = 5'd0;
+
+	#100;
+	func = 3'b010;
+	shamt = 5'd0;
+
+	#100; 
+	a = 32'd4;
+	b = 32'd3;
+	func = 3'b011;
+	shamt = 5'd0;
+	
+	#100;
+	a = 32'd2;
+	b = 32'd3;
+	func = 3'b100;
+	shamt = 5'd0;
+
+	#100;
+	b = 32'd1;
+	func = 3'b101;
+	shamt = 5'd1;
+
+	#100;
+	b = 32'd2;
+	func = 3'b110;
+	shamt = 5'd1;
+
+	#100;
+	a = 32'd2;
+	b = 32'd3;
+	func = 3'b111;
+	shamt = 5'd0;
+
+end
+endmodule
